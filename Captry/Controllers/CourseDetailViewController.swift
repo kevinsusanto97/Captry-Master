6b79af4fae049cd23ef5987ccf41741e9a435ecc
Fauzan Achmad <fauzan.achmad@student.umn.ac.id>
2019-07-12 17:57:49 +0700

    course detail in progress

---
+++ Captry/Controllers/CourseDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CourseDetailViewController.swift
+//  Captry
+//
+//  Created by Fauzan Achmad on 12/07/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class CourseDetailViewController: UIViewController {
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var overviewContent: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        overviewContent.lineBreakMode = .byWordWrapping
+        overviewContent.numberOfLines = 0
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
