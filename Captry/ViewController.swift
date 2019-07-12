43176dd4b809982eb6434013317413459769a067
Kevin Susanto <kevin.stnt@gmail.com>
2019-07-12 12:53:10 +0700

    Initial Commit

---
+++ Captry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Captry
+//
+//  Created by Kevin Susanto on 12/07/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
