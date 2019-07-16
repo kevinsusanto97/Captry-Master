4b1949002b220e39fec9c960f96e6778b9312db5
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-07-16 21:34:27 +0700

    Add CourseData Class to modify data, so label, image and video link for all Course in CourseDetailViewController can be changed

--- Captry/Controllers/CourseDetailViewController.swift
+++ Captry/Controllers/CourseDetailViewController.swift
@@ -15,27 +15,111 @@ class CourseDetailViewController: UIViewController {
     @IBOutlet weak var overviewLabel: UILabel!
     @IBOutlet weak var overviewContent: UILabel!
     @IBOutlet weak var playerView: WKYTPlayerView!
-    let course: [[String]] = [
-        [
-            "Rule of Thirds",
-            "In this course you will learn about how to place some object into grids. Rule of third is a popular composition techniqu",
-            "xTI09ckSqys"
-        ],
-        [
-            "Symetry",
-            "Symetry content",
-            "h1nWJHK242Q"
-        ]
-    ]
+    @IBOutlet weak var referencesImage: UIImageView!
     
     var temp: String?
+    
+    var data = CoursesData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationItem.title = temp
-        playerView.load(withVideoId: "xTI09ckSqys")
-        overviewContent.text = ""
+//        overviewContent.text = ""
+        changeLabel()
+        changeVideo()
+        addImageToImageView()
     }
     
+    func changeView()
+    {
+        var listImage = data.ruleOfThirdImage
+        var videoLink = data.ruleOfThirdVideo
+        
+        if temp == "Rule of Thirds"
+        {
+            overviewContent.text = data.ruleOfThirdOverview
+        }
+        else if temp == "Symetry"
+        {
+            overviewContent.text = "Simetris"
+            listImage = data.symetryImage
+            videoLink = data.symetryVideo
+            
+        }
+        else if temp == "Triangle Ratio"
+        {
+            overviewContent.text = "Segitiga"
+        }
+        else if temp == "Golden Ratio"
+        {
+            overviewContent.text = "Emas"
+        }
+    }
+    func changeLabel()
+    {
+        if temp == "Rule of Thirds"
+        {
+            overviewContent.text = data.ruleOfThirdOverview
+        }
+        //overviewContent.text = "Title"
+        else if temp == "Symetry"
+        {
+            overviewContent.text = "Simetris"
+        }
+        else if temp == "Triangle Ratio"
+        {
+            overviewContent.text = "Segitiga"
+        }
+        else if temp == "Golden Ratio"
+        {
+            overviewContent.text = "Emas"
+        }
+    }
+    
+    func addImageToImageView()
+    {
+        var listImage = data.ruleOfThirdImage
+        if temp == "Rule of Thirds"
+        {
+            listImage = data.ruleOfThirdImage
+        }
+        else if temp == "Symetry"
+        {
+            listImage = data.symetryImage
+        }
+        else if temp == "Triangle Ratio"
+        {
+            listImage = data.goldenTriangleImage
+        }
+        else if temp == "Golden Ratio"
+        {
+            listImage = data.goldenRatioImage
+        }
+        let yourImage: UIImage = UIImage(named: listImage)!
+        referencesImage.image = yourImage
+    }
+    
+    func changeVideo()
+    {
+        var videoLink = data.ruleOfThirdVideo
+        if temp == "Rule of Thirds"
+        {
+            videoLink = data.ruleOfThirdVideo
+        }
+        else if temp == "Symetry"
+        {
+            videoLink = data.symetryVideo
+        }
+        else if temp == "Triangle Ratio"
+        {
+            videoLink = data.goldenTriangleVideo
+        }
+        else if temp == "Golden Ratio"
+        {
+            videoLink = data.goldenRatioVideo
+        }
+        
+        playerView.load(withVideoId: videoLink)
+    }
 }
