8d98074044d998ca3f075991e145aae969d86cf2
Kevin Susanto <kevin.stnt@gmail.com>
2019-07-17 15:15:19 +0700

    Custom Button Class & Fix Asset

--- Captry/Views/CustomUI.swift
+++ Captry/Views/CustomUI.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class CustomUI: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class UISecondaryButton: UIButton {
+    override func didMoveToWindow() {
+        self.layer.cornerRadius = 5
+        self.layer.borderWidth = 0.75
+        self.layer.borderColor = #colorLiteral(red: 1, green: 0.6498119235, blue: 0, alpha: 1)
+        self.tintColor = #colorLiteral(red: 1, green: 0.6498119235, blue: 0, alpha: 1)
     }
-    */
+}
 
+class UIPrimaryButton: UIButton {
+    override func didMoveToWindow() {
+        self.layer.cornerRadius = 5
+        self.layer.backgroundColor = #colorLiteral(red: 1, green: 0.6498119235, blue: 0, alpha: 1)
+        self.tintColor = .white
+    }
 }
