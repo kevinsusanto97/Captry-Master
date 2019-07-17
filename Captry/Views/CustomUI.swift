945868b7cc05fd1cf799ede1d321a37b0a9b858a
Kevin Susanto <kevin.stnt@gmail.com>
2019-07-17 15:05:37 +0700

    Create CustomUI Class

---
+++ Captry/Views/CustomUI.swift
@@ -0,0 +1,21 @@
+//
+//  CustomUI.swift
+//  Captry
+//
+//  Created by Kevin Susanto on 17/07/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class CustomUI: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
