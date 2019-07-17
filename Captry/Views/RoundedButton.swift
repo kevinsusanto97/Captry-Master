a01ed82c3db0c3eb67bc8fc6cbed0f98d7726c8a
Annisa Nabila Nasution <annisa.nasution@binus.ac.id>
2019-07-17 15:40:23 +0700

    adding back button and fixing flash image

---
+++ Captry/Views/RoundedButton.swift
@@ -0,0 +1,17 @@
+//
+//  RoundedButton.swift
+//  Captry
+//
+//  Created by Annisa Nabila Nasution on 17/07/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    override func didMoveToWindow() {
+        self.layer.cornerRadius = self.frame.height / 2
+    }
+
+}
