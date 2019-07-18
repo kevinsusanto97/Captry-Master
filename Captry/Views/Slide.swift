3dd447c175a91c88f5362bb6f1296c985a28af6f
Fauzan Achmad <fauzan.achmad@student.umn.ac.id>
2019-07-18 23:47:07 +0700

    rebase to master

---
+++ Captry/Views/Slide.swift
@@ -0,0 +1,17 @@
+//
+//  Slide.swift
+//  Captry
+//
+//  Created by Fauzan Achmad on 18/07/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelTitle: UITextView!
+    @IBOutlet weak var labelDesc: UITextView!
+    
+}
