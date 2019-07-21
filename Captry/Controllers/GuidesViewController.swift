59c6773dba1387668df74ac11a7ae37aac4dc096
Kevin <kevin.stnt@gmail.com>
2019-07-21 18:39:50 +0700

    Add ScrollView to guides

---
+++ Captry/Controllers/GuidesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GuidesViewController.swift
+//  Captry
+//
+//  Created by Kevin Susanto on 21/07/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class GuidesViewController: UIView {
+
+    @IBOutlet var scrollViewGuides: UIScrollView!
+    @IBOutlet var pageControlGuides: UIPageControl!
+    
+    
+    
+}
+
+
